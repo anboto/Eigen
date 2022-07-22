ee6ee274b0cdd51238711ea7eb1e2c02fca9b7cd
Iñaki <izabala123@gmail.com>
2022-07-22 15:47:14 +0200

    First release

---
+++ Eigen/ToStringPlugin.h
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2021 - 2022, the Anboto author and contributors
+	
+UPP::String ToString() const {
+	std::stringstream s;
+	s << *this;
+	return s.str();
+}
+
