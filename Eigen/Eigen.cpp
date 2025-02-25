b4ab4d839393e870668996f86df3409f9fbe9607
Iñaki Zabala <izabala123@gmail.com>
2025-02-25 11:14:52 +0100

    Cosmetic change

--- Eigen/Eigen.cpp
+++ Eigen/Eigen.cpp
@@ -64,8 +64,8 @@ double SolveNonLinearEquation(double y, Function <double(double b)> Residual, do
 	return Null;		
 }
 
-Eigen::Matrix3d SkewSymmetricMatrix(const Eigen::Vector3d& r) {
-    Eigen::Matrix3d s;
+Matrix3d SkewSymmetricMatrix(const Vector3d& r) {
+    Matrix3d s;
     s << 0, -r.z(), r.y(),
          r.z(), 0, -r.x(),
         -r.y(), r.x(), 0;
